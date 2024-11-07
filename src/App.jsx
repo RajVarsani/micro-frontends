a282f3f67e3cf6265f01e87c5998e9fc842f3c0b
Raj Varsani <varasaniraj5@gmail.com>
2024-11-07 14:24:25 +0530

    initialize micro-frontend project with React, Webpack, and Babel setup

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import Home from "home/app";
+import Dashboard from "dashboard/app";
+
+import "./index.css";
+
+const App = () => (
+  <div className="container">
+    <div>Name: micro-frontends</div>
+    <div>Framework: react</div>
+    <div>Language: JavaScript</div>
+    <div>CSS: Empty CSS</div>
+    {/* <Home /> */}
+    <Dashboard />
+  </div>
+);
+const rootElement = document.getElementById("app");
+if (!rootElement) throw new Error("Failed to find the root element");
+
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(<App />);
